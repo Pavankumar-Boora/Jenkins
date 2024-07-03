c850d7220613cedcacd1e3b9c3f1f73f79b02170
Pavankumar-Boora <pavankumarboora29@gmail.com>
2024-07-03 22:25:10 +0530

    updating Spy programm

---
+++ Spy.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Spy 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter no. for which uh want start finding SPY number ");
+		int m=s.nextInt();
+		System.out.println("Enter no. for which uh want end finding SPY number ");
+		int n=s.nextInt();
+		for(int i=m;i<=n;i++){
+			int num=i,product=1,sum=0;
+			while(num!=0){
+				int rem=num%10;
+				sum+=rem;
+				product*=rem;
+				num/=10;
+			}
+			if(sum==product){
+				System.out.println(i);
+			}
+		} */
+		System.out.println("Spy class is loading");
+	}
+}
